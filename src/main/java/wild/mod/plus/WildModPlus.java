1c95841f6c88d880957ff4ffd0a1fa748eeb1487
Osmiooo <owencraft2000@gmail.com>
2021-12-13 20:41:31 +0100

    Initial commit

---
+++ src/main/java/wild/mod/plus/WildModPlus.java
@@ -0,0 +1,11 @@
+package wild.mod.plus;
+
+import net.fabricmc.api.ModInitializer;
+
+public class WildModPlus implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
