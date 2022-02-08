a71ea9524e610848fb7993aab640d6fd3e5b3c6f
Xfrtrex <XfrPLayz@gmail.com>
2022-02-08 13:04:47 -0700

    more deleted stuff

--- src/main/java/wild/mod/plus/entity/AllayEntity.java
+++ src/main/java/wild/mod/plus/entity/AllayEntity.java
@@ -2,19 +2,13 @@ package wild.mod.plus.entity;
 
 import net.minecraft.entity.*;
 import net.minecraft.entity.ai.control.FlightMoveControl;
-import net.minecraft.entity.ai.control.MoveControl;
 import net.minecraft.entity.ai.goal.LookAtEntityGoal;
 import net.minecraft.entity.ai.pathing.PathNodeType;
 import net.minecraft.entity.passive.AnimalEntity;
 import net.minecraft.entity.passive.PassiveEntity;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Box;
-import net.minecraft.util.math.MathHelper;
-import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 import net.minecraft.world.WorldView;
 import org.jetbrains.annotations.Nullable;
@@ -52,48 +46,4 @@ public class AllayEntity extends AnimalEntity implements Flutterer {
     public PassiveEntity createChild(ServerWorld world, PassiveEntity entity) {
         return null;
     }
-
-    static class AllayMoveControl extends MoveControl {
-        private final AllayEntity allay;
-        private int collisionCheckCooldown;
-
-
-        public AllayMoveControl(AllayEntity allay) {
-            super(allay);
-            this.allay = allay;
-        }
-
-        public void tick() {
-            if (this.state == MoveControl.State.MOVE_TO) {
-                if (this.collisionCheckCooldown-- <= 0) {
-                    this.collisionCheckCooldown += this.allay.getRandom().nextInt(5) + 2;
-                    Vec3d vec3d = new Vec3d(this.targetX - this.allay.getX(), this.targetY - this.allay.getY(), this.targetZ - this.allay.getZ());
-                    double d = vec3d.length();
-                    vec3d = vec3d.normalize();
-                    if (this.willCollide(vec3d, MathHelper.ceil(d))) {
-                        this.allay.setVelocity(this.allay.getVelocity().add(vec3d.multiply(0.1D)));
-                    } else {
-                        this.state = MoveControl.State.WAIT;
-
-
-                    }
-                }
-
-            }
-
-        }
-
-        private boolean willCollide(Vec3d direction, int steps) {
-            Box box = this.allay.getBoundingBox();
-
-            for (int i = 1; i < steps; ++i) {
-                box = box.offset(direction);
-                if (!this.allay.world.isSpaceEmpty(this.allay, box)) {
-                    return false;
-                }
-            }
-
-            return true;
-        }}
-
 }
