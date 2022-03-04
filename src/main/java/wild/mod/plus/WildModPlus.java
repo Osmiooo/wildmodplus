0897928b93bca676bd4dc7eb444a80610b5d632a
Xfrtrex <XfrPLayz@gmail.com>
2022-03-04 15:21:33 -0700

    bur

--- src/main/java/wild/mod/plus/WildModPlus.java
+++ src/main/java/wild/mod/plus/WildModPlus.java
@@ -30,6 +30,8 @@ public class WildModPlus implements ModInitializer {
 
     public static final GameEvent JAW_ACTIVATE = new GameEvent("jaw_activate", 16);
 
+    public static final GameEvent SCULK_ECHOER_ECHO = new GameEvent("sculk_echoer_echo", 16);
+
     public static final EntityType<AllayEntity> ALLAY = Registry.register(
             Registry.ENTITY_TYPE,
             new Identifier(WildModPlus.MOD_ID, "allay"),
@@ -42,6 +44,7 @@ public class WildModPlus implements ModInitializer {
     public void onInitialize() {
         RegisterBlocks.RegisterBlocks();
         Registry.register(Registry.GAME_EVENT, new Identifier(WildModPlus.MOD_ID, "jaw_activate"), JAW_ACTIVATE);
+        Registry.register(Registry.GAME_EVENT, new Identifier(WildModPlus.MOD_ID, "sculk_echoer_echo"), SCULK_ECHOER_ECHO);
         Registry.register(Registry.ITEM, new Identifier(WildModPlus.MOD_ID, "allay_spawn_egg"), ALLAY_SPAWN_EGG);
         RegisterSounds.RegisterSounds();
         FabricDefaultAttributeRegistry.register(ALLAY, AllayEntity.createMobAttributes());
--- src/main/java/wild/mod/plus/blocks/SculkEchoerBlock.java
+++ src/main/java/wild/mod/plus/blocks/SculkEchoerBlock.java
@@ -1,16 +1,25 @@
 package wild.mod.plus.blocks;
 
+import frozenblock.wild.mod.WildMod;
 import frozenblock.wild.mod.registry.RegisterSounds;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.*;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.sound.BlockSoundGroup;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.util.Identifier;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.intprovider.UniformIntProvider;
+import net.minecraft.util.registry.Registry;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.util.shape.VoxelShapes;
 import net.minecraft.world.BlockView;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import wild.mod.plus.WildModPlus;
 
 public class SculkEchoerBlock extends Block {
     private static final VoxelShape SHAPE = VoxelShapes.union(Block.createCuboidShape(0.0D, 0.0D, 0.0D, 16.0D, 8.0D, 16.0D), Block.createCuboidShape(1.0D, 8.0D, 1.0D, 15.0D, 16D, 15.0D));
@@ -24,6 +33,23 @@ public class SculkEchoerBlock extends Block {
         super.onStacksDropped(state, world, pos, stack);
         int i = 20;
         this.dropExperience(world, pos, i);
+    }
+
+    @Override
+    public void onEntityCollision(BlockState state, World world, BlockPos pos, Entity entity) {
+        Identifier WARDEN = new Identifier(WildMod.MOD_ID, "warden");
+        if (entity instanceof LivingEntity && !Registry.ENTITY_TYPE.getId(entity.getType()).equals(WARDEN)) {
+            world.emitGameEvent(entity, WildModPlus.SCULK_ECHOER_ECHO, pos);
+            world.playSound(
+                    null,
+                    pos,
+                    wild.mod.plus.registry.RegisterSounds.BLOCK_SCULK_ECHOER_RECEIVE_VIBRATION,
+                    SoundCategory.BLOCKS,
+                    1.0f,
+                    world.random.nextFloat() * 0.1F + 0.9F
+            );
+        }
+        super.onEntityCollision(state, world, pos, entity);
     }
 
     @Override
